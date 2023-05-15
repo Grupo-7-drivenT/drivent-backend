78c615f0c1d596b150e27870bd42fd6a6ed553ce
Dev-organico <joao.zolet@gmail.com>
2023-05-15 14:45:19 -0300

    feat: add hotel and room seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,11 +7,11 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
   }
@@ -19,23 +19,68 @@ async function main() {
   await prisma.ticketType.createMany({
     data: [
       {
-        name: "remoto",
+        name: 'remoto',
         price: 10000,
         isRemote: true,
-        includesHotel: false
+        includesHotel: false,
       },
       {
-        name: "presencial",
+        name: 'presencial',
         price: 25000,
         isRemote: false,
-        includesHotel: false
+        includesHotel: false,
       },
       {
-        name: "com hotel",
+        name: 'com hotel',
         price: 60000,
         isRemote: false,
-        includesHotel: true
-      }
+        includesHotel: true,
+      },
+    ],
+  });
+
+  const hotel_1 = await prisma.hotel.create({
+    data: {
+      name: 'Driven Palace',
+      image: 'https://dicasdacalifornia.com.br/wp-content/uploads/2019/03/caesars-palace-las-vegas.jpg',
+    },
+  });
+
+  const hotel_2 = await prisma.hotel.create({
+    data: {
+      name: 'Driven tower',
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/city-skyline-at-night-with-bellagio-hotel-water-royalty-free-image-1670351062.jpg?crop=1.00xw:0.713xh;0,0.112xh&resize=1200:*',
+    },
+  });
+
+  await prisma.room.createMany({
+    data: [
+      {
+        name: 'Single',
+        capacity: 1,
+        hotelId: hotel_1.id,
+      },
+      {
+        name: 'Double',
+        capacity: 2,
+        hotelId: hotel_1.id,
+      },
+      {
+        name: 'Single',
+        capacity: 1,
+        hotelId: hotel_2.id,
+      },
+      {
+        name: 'Double',
+        capacity: 2,
+        hotelId: hotel_2.id,
+      },
+      {
+        name: 'Triple',
+        capacity: 3,
+        hotelId: hotel_2.id,
+      },
     ],
   });
 
