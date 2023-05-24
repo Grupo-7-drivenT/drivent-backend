8f08de49a33c24368cd61593566be0b79c0c4df7
Hugo Junior <hjunioreng@gmail.com>
2023-05-24 19:16:56 -0300

    Fix migration

--- src/repositories/activity-repository/index.ts
+++ src/repositories/activity-repository/index.ts
@@ -1,17 +1,35 @@
 import { prisma } from '@/config';
 
-async function findActivities() {
-  return prisma.activity.findMany();
-}
-
 async function findActivityId(id: number) {
   return prisma.activity.findFirst({
     where: { id },
   });
 }
+async function findActivitySectionalId(locationId: number) {
+  return prisma.activity.findMany({
+    where: { locationId },
+  });
+}
+async function findActivities() {
+  return prisma.activity.findMany();
+}
+async function findSectional() {
+  return prisma.sectional.findMany();
+}
+async function findSectionalId(id: number) {
+  return prisma.sectional.findFirst({
+    where: {
+      id,
+    },
+  });
+}
+
 const activityRepository = {
+  findSectional,
+  findSectionalId,
   findActivityId,
   findActivities,
+  findActivitySectionalId,
 };
 
 export default activityRepository;
--- src/services/activities-service/index.ts
+++ src/services/activities-service/index.ts
@@ -1,6 +1,18 @@
 import dayjs from 'dayjs';
 import activityRepository from '@/repositories/activity-repository';
 
+async function findActivityIdService(activityId: number) {
+  try {
+    const activityById = activityRepository.findActivityId(+activityId);
+    if (!activityById) {
+      throw new Error('');
+    }
+    const location = activityRepository.findSectional();
+    return activityById;
+  } catch (error) {
+    console.log(error);
+  }
+}
 async function findActivitiesService() {
   const allActivities = activityRepository.findActivities();
   return allActivities;
